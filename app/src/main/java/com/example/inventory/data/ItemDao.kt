fc6257929ef419fd0f2e048cb66fb8af37f54e06
sbdjay <piusd@shernet.sheridancollege.ca>
2023-11-25 18:12:41 -0500

    Item DAO Added and Completed

---
+++ app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -0,0 +1,31 @@
+package com.example.inventory.data
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Update
+import androidx.room.Delete
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+
+
+
+@Dao
+interface ItemDao {
+    @Insert (onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(item:Item)
+
+    @Update
+    suspend fun update(item: Item)
+
+    @Delete
+    suspend fun delete(item: Item)
+
+    @Query("SELECT * FROM items WHERE id = :id")
+    fun getItem(id: Int): Flow<Item>
+
+    @Query("SELECT * FROM items ORDER BY  name ASC")
+    fun getAllItems(): Flow<List<Item>>
+
+}
+
